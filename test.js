d0ad1a56b6adb353cb07ddd8ff3236e1076f40d9
Gudiya_Kumari <124147532+GudiyaGaur@users.noreply.github.com>
2023-10-01 17:18:34 +0530

    Add files via upload

--- test.js
+++ test.js
@@ -1,1 +1,40 @@
 console.log("gudiya");
+
+// variable
+
+const accountId = 12355;
+let accountEmail = "gudiya@google.com";
+let accountPassword = "12345";
+accountCity = "patna";
+let accountState;
+
+console.log(accountId);
+console.log([
+  accountId,
+  accountEmail,
+  accountPassword,
+  accountCity,
+  accountState,
+]);
+
+/* 
+var is not used
+ because of block or scope of code
+*/
+
+("use strict"); // treat as new version
+
+//datatype: primitive & non- premitive
+
+/*
+ -- number
+ -- string
+ -- boolean
+ -- null :stand alone value
+ -- undefind : initialized 
+ -- symbol
+
+ -- object
+ -- array
+*/
+console.log(typeof null);
