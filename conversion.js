d48e55ceaa85d9c462ffe65eb825df9a9ab6421b
Gudiya_Kumari <124147532+GudiyaGaur@users.noreply.github.com>
2023-10-01 17:19:23 +0530

    Add files via upload

---
+++ conversion.js
@@ -0,0 +1,12 @@
+"use strict";
+
+let score = "33";
+let valueInNumber = Number(score);
+//console.log(typeof score);
+//console.log(typeof valueInNumber);
+
+// "33abc" => NaN
+
+let name = 123;
+let chaneInString = String(name);
+console.log(typeof chaneInString);
